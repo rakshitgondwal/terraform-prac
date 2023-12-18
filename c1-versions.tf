d83fb0bcd03719a021799232689a2bbdff43b196
Rakshit Gondwal <rakshitgondwal3@gmail.com>
2023-12-18 14:11:31 +0530

    initail commit
    Signed-off-by: Rakshit Gondwal <rakshitgondwal3@gmail.com>

---
+++ c1-versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = "~> 1.6.6"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = us-east-1
+  profile = "default"
+}
